228bf9458898ab72fd4d352d37166a6dcdd90849
Enes Kaya <eng.eneskaya@gmail.com>
2022-08-06 13:24:25 +0300

    no message

--- ProtocolProject/main.swift
+++ ProtocolProject/main.swift
@@ -9,3 +9,34 @@ import Foundation
 
 print("Hello, World!")
 
+protocol Running{
+    func pRunning()
+    
+}
+
+class Animal{
+    
+    func running(){
+        print("run")
+    }
+}
+
+class Cat : Animal,Running{
+    func pRunning() {
+        print("run")
+    }
+    
+        
+}
+
+
+let badem = Cat()
+
+
+badem.running()
+badem.pRunning()
+
+
+
+
+
